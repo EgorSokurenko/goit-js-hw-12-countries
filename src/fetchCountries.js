13cdcdc2b9473133e8240c747095483236e56b10
EgorSokurenko <72275136+EgorSokurenko@users.noreply.github.com>
2021-09-27 23:47:48 +0300

    вроде бы все

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,17 +1,21 @@
-import { error } from "@pnotify/core";
+import { alert, error, defaultModules } from '@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+defaultModules.set(PNotifyMobile, {});
 
-export default function fetchCountries(searchQuery){
-    return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
-        .then(res=>{
-            
-            if(res.ok!=true){
-                return reject()
-                
-            }else if(res.ok===false){
-                reject('loшпед')
-                
-            }
-            return res.json()})
-            .catch(error=>{console.log(error)})
-        }
-    
+export default function fetchCountries(searchQuery) {
+  return fetch(`https://restcountries.com/v3/name/${searchQuery}`)
+    .then(res => {
+      if (res.ok != true) {
+        error({ text: 'Некоректно' });
+        return;
+      } else if (res.ok === false) {
+        reject('loшпед');
+      }
+      return res.json();
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
--- src/index.js
+++ src/index.js
@@ -1,56 +1,45 @@
 import './sass/main.scss';
-import conuntrySample from './partials/country'
-import conuntrysSample from './partials/countrys'
-import fetchCountries from './fetchCountries'
-import notie from 'notie'
-import { VERSION } from 'lodash';
+import conuntrySample from './partials/country';
+import conuntrysSample from './partials/countrys';
+import fetchCountries from './fetchCountries';
+// import notie from 'notie';
+// import { VERSION } from 'lodash';
 var debounce = require('lodash.debounce');
-var PNotify = require('@pnotify/core');
+import { alert, error, defaultModules } from '@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+defaultModules.set(PNotifyMobile, {});
 
+const input = document.querySelector('#country-input');
+const list = document.querySelector('#list-country');
+const listTWo = document.querySelector('#list-two');
 
+input.addEventListener('input', debounce(searchCountrys, 500));
 
+function searchCountrys(evt) {
+  let country = document.querySelector('#country');
+  if (evt.target.value === '') {
+    list.removeChild(country);
+    return;
+  } else if (country) {
+    list.removeChild(country);
+  }
 
-
-
-
-const input = document.querySelector('#country-input')
-const list = document.querySelector('#list-country')
-const listTWo = document.querySelector('#list-two')
-
-input.addEventListener('input', debounce(searchCountrys,500))
-
-function searchCountrys(evt){
-    let country = document.querySelector('#country')
-    if(evt.target.value === ''){
-        list.removeChild(country)
-        return
-    }else if(country){
-      list.removeChild(country)   
-    }
-    let valueInp = evt.target.value
-    fetchCountries(valueInp)
-    .then(
-        country=>{
-            
-            if(country.length === 1){
-               
-                list.innerHTML=conuntrySample(country)
-            }else if(country.length>1&&country.length<=10){
-                list.innerHTML=conuntrysSample(country)
-                
-            }else if(country.length>10){
-                reject('There was an error :(');
-                
-                    
-            }
-        })
-    .catch(error=>{
-        
-        console.log(error);
-        notie.alert({ type: 3, text: 'Произошла ошибочка :(', position: 'bottom' })
-        })
-}
-
-function errorLog(){
-    notie.alert({ type: 3, text: 'Произошла ошибочка :', position: 'bottom' })
+  let valueInp = evt.target.value;
+  fetchCountries(valueInp)
+    .then(country => {
+      console.log(country);
+      let link = country[0].flags[0];
+      if (country.length === 1) {
+        list.innerHTML = conuntrySample({ country, link });
+      } else if (country.length > 1 && country.length <= 10) {
+        list.innerHTML = conuntrysSample(country);
+      } else if (country.length > 10) {
+        error({ text: 'Too many matches found. Please enter a more specific query!' });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
 }
